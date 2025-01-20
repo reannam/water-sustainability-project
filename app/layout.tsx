6ff3ce45b4b493394132472ebfc372a85ab8c030
Egal97 <egallagher97@gmail.com>
2025-01-20 19:31:22 +0000

    remove code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { NavBar } from "./components/NavBar";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -24,10 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body>
+        <main className="min-h-screen">
+          <NavBar />
+          <div className="flex flex-col grow p-8">{children}</div>
+        </main>
       </body>
     </html>
   );
