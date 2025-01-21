4200bb5b60d2119ee88a938c0bb86e2def1a85c0
reannam <reannamacd@gmail.com>
2025-01-21 20:16:52 +0000

    home pg changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,42 +2,52 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <html>
-      <body>
-        <div className="image-row">
+    <main>
+      <div className="image-row">
+        <div className="image-container">
           <Image 
             src="/images/loughNeagh.jpg" 
             alt="Lough Neagh"
-            width={200}
-            height={200}
-            className="image-border"  // Add the border class here
+            width={300}
+            height={300}
+            className="image-border"
           />
+          <button className="image-button">Lough Neagh</button>
+        </div>
+        <div className="image-container">
           <Image 
             src="/images/plasticPollution.jpg" 
             alt="Plastic Pollution"
-            width={200}
-            height={200}
-            className="image-border"  // Add the border class here
+            width={300}
+            height={300}
+            className="image-border"
           />
+          <button className="image-button">Plastic Pollution</button>
+        </div>
+        <div className="image-container">
           <Image 
             src="/images/manureRunoff.jpg" 
             alt="Agricultural Pollution"
-            width={200}
-            height={200}
-            className="image-border"  // Add the border class here
+            width={300}
+            height={300}
+            className="image-border"
           />
+          <button className="image-button">Agricultural Pollution</button>
+        </div>
+        <div className="image-container">
           <Image 
             src="/images/historicalDataImage.jpg" 
             alt="Historical"
-            width={200}
-            height={200}
-            className="image-border"  // Add the border class here
+            width={300}
+            height={300}
+            className="image-border"
           />
+          <button className="image-button">Historical</button>
         </div>
-        <div>
-          <h1>Get Involved!</h1>
-        </div>
-      </body>
-    </html>
+      </div>
+      <div>
+        <h1 className="get-involved-heading">Get Involved!</h1>
+      </div>
+    </main>
   );
 }
