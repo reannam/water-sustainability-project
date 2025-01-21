ce83ba82ccfce28430d06e20c1f6d7dcac798d74
reannam <reannamacd@gmail.com>
2025-01-21 20:18:50 +0000

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <NavBar />
-      </body>
-      <body>{children}</body>
+      <NavBar />
+     <body>{children}
+
+     
+     </body>
+        
     </html>
   );
 }
