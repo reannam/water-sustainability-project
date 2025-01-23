2d022a8ef693b190c8fe8891bfbe38c7ad0cb836
SneakySquid87 <philipGardinerm00@gmail.com>
2025-01-23 17:49:25 +0000

    V0 My Beloved

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -50,17 +50,39 @@ export default {
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
   			},
-			  sky: {
-				50: '#f0f9ff',
-				100: '#e0f2fe',
-				600: '#0284c7',
-				700: '#0369a1',
-			  }
+  			sky: {
+  				'50': '#f0f9ff',
+  				'100': '#e0f2fe',
+  				'600': '#0284c7',
+  				'700': '#0369a1'
+  			}
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
