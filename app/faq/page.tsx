b5860e6ccec7f76cc8c848067f4380bcad1be4c7
40197782 <40197782@eeecs.qub.ac.uk>
2025-01-23 19:44:35 +0000

    colour changes to pages, FAQ added, link added to footer, torquise added to titles

--- app/faq/page.tsx
+++ app/faq/page.tsx
@@ -1,16 +1,16 @@
 "use client";
-import FAQAccordion from '@/components/faq-accordion'
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import FAQAccordion from "./faq-accordion";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function FaqPage() {
   return (
-    <main>
-      <h1>Welcome to our site</h1>
-      {/* Other content */}
-      <FAQAccordion />
-    </main>
-  )
+    <div>
+        <img 
+          src="/logo.png" 
+          alt="Aqua Alert NI Logo" 
+          className="logo" 
+        />
+        <FAQAccordion />
+  </div>
+);
 }
